0f2c0480b680152dbca0529c90f7e68187b61a7f
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:54:03 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: 'skt상품권 희망티켓',
+  siteUrl: 'https://gift-hh-1.pages.dev',
+  siteDescription: 'skt상품권 모바일 매입 전문. SK 통신사 발행 상품권을 업계 최고가 시세로 5분 내 초고속 정산해 드립니다.',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
